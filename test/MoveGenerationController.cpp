817f221b37731d407e8435bd4021da8f83b829f5
dansmith <dsmith@ltu.edu>
2016-09-02 00:11:03 -0400

    Added second test. Its failing

--- test/MoveGenerationController.cpp
+++ test/MoveGenerationController.cpp
@@ -23,7 +23,7 @@
 		copy(begin(newBoardArray), end(newBoardArray), begin(board->squares));
 	}
 
-	TEST_F(MoveGenerationControllerTest, RunFirstTest) {
+	TEST_F(MoveGenerationControllerTest, KingInCorner) {
 		board->squares[0] = WHITE_KING;
 		moveGenerationController->generateMovesAt(0);
 		EXPECT_EQ(moveStack->top, 3);
@@ -34,4 +34,27 @@
 		EXPECT_EQ(TO(moveStack->stack[0]), 1);
 		EXPECT_EQ(TO(moveStack->stack[1]), 16);
 		EXPECT_EQ(TO(moveStack->stack[2]), 17);
+	}
+
+	TEST_F(MoveGenerationControllerTest, KingInMiddle) {
+		board->squares[17] = WHITE_KING;
+		moveGenerationController->generateMovesAt(17);
+		EXPECT_EQ(moveStack->top, 8);
+		sort(moveStack->stack, moveStack->stack + 8);
+		EXPECT_EQ(FROM(moveStack->stack[0]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[1]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[2]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[3]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[4]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[5]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[6]), 17);
+		EXPECT_EQ(FROM(moveStack->stack[7]), 17);
+		EXPECT_EQ(TO(moveStack->stack[0]), 0);
+		EXPECT_EQ(TO(moveStack->stack[1]), 1);
+		EXPECT_EQ(TO(moveStack->stack[2]), 2);
+		EXPECT_EQ(TO(moveStack->stack[3]), 16);
+		EXPECT_EQ(TO(moveStack->stack[4]), 18);
+		EXPECT_EQ(TO(moveStack->stack[5]), 32);
+		EXPECT_EQ(TO(moveStack->stack[6]), 33);
+		EXPECT_EQ(TO(moveStack->stack[7]), 34);
 	}
