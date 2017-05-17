906c68ac3e5bf1ac9e89b3bc2bef43118099e747
dansmith <dsmith@ltu.edu>
2017-05-17 00:23:49 -0400

    [bitboad-rework] Passing perft up to depth 5.

--- test/Perft.cpp
+++ test/Perft.cpp
@@ -25,34 +25,20 @@
 		performPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3, 8902);
 		performPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281);
 		performPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5, 4865609);
+		// performPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, 119060324);
 	}
 
 	TEST_F(PerftTest, Kiwipete) {
-		// performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 1, 48);
-		// performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 2, 2039);
-		// performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 7, 97862);
+		performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 1, 48);
+		performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 2, 2039);
+		// performPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 3, 97862);
 	}
 
 	TEST_F(PerftTest, Position3) {
-		// performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 1, 14);
-		// performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 2, 191);
-	}
-
-	TEST_F(PerftTest, EngineComparison) {
-		performPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5, 4865609);
-		// performPerft("rnbqkbnr/pppppppp/8/8/P7/8/1PPPPPPP/RNBQKBNR b KQkq a3", 4, 217832);
-		// performPerft("rnbqkbnr/1ppppppp/p7/8/P7/8/1PPPPPPP/RNBQKBNR w KQkq -", 3, 9312);
-		// performPerft("rnbqkbnr/1ppppppp/p7/P7/8/8/1PPPPPPP/RNBQKBNR b KQkq -", 2, 380);
-		// rnbqkbnr/2pppppp/p7/Pp6/8/8/1PPPPPPP/RNBQKBNR w KQkq b6
-		// "rnbqkbnr/2pppppp/p7/PP6/1P6/8/2PPPPPP/RNBQKBNR b KQkq b3"
-		// vector<string> outputFens;
-		// Fen::import("rnbqkbnr/1ppppppp/p7/P7/8/8/1PPPPPPP/RNBQKBNR b KQkq -");
-		// Board::doMove(17993);
-		// Board::doMove(18545);
-		// cout << Fen::getBoardSquares() << endl;
-		// MoveGenerationController::logPath(2);
-		// 17993
-		// rnbqkbnr/2pppppp/p7/Pp6/8/8/1PPPPPPP/RNBQKBNR w KQkq b6
-		// 18545
-		// rnbqkbnr/2pppppp/p7/PP6/1P6/8/2PPPPPP/RNBQKBNR b KQkq b3
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 1, 14);
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 2, 191);
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 3, 2812);
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 4, 43238);
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 5, 674624);
+		performPerft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 6, 11030083);
 	}
