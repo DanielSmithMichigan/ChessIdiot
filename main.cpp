f1085473b71e1c1cf16568403ea9898d40d6264d
danielsmithmichigan <dansmith@quickenloans.com>
2018-06-25 19:05:41 -0400

    Re-implemented benchmarking

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	ConstructUci::instance->fromArgv(argc, argv);
-	// Uci::instance->loop();
-	// Search::instance->iterativeDeepening("r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1", 99);
-	// Fen::import("r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1");
-	// MoveGenerationController::instance->getBestMove(7);
+	Uci::instance->loop();
+    // Init::instance->execute();
+    // Fen::import("r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1");
+    // Search::instance->iterativeDeepening(5);
     return 0;
 }
